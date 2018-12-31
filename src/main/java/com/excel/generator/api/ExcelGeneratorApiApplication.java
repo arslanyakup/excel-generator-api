01c1128dd2f58306607d8935268be92185246afd
yakup.arslan <yakup.arslan@eteration.com>
2018-12-31 22:55:56 +0300

    happy new year. :)

---
+++ src/main/java/com/excel/generator/api/ExcelGeneratorApiApplication.java
@@ -0,0 +1,14 @@
+package com.excel.generator.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelGeneratorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelGeneratorApiApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/excel/generator/api/ExcelGeneratorApiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.excel.generator.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExcelGeneratorApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
